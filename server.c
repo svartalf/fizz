a0a54cf9ba2e0624049be86c004190bb7a0a7275
SvartalF <self@svartalf.info>
2012-07-06 12:59:04 +0900

    Callback for writing into client sockets

--- server.c
+++ server.c
@@ -12,11 +12,33 @@
 #include <ev.h>
 
 /**
+ * When the client socket will become writeable, send to him new ID
+ */
+static void write_callback(EV_P, ev_io *w, int revents) {
+	// TODO: check for EV_ERROR & revents
+	send(w->fd, "test", 4, 0);
+
+	// TODO: check for correct way of socket closing and loop stopping
+	close(w->fd);
+	ev_io_stop(loop, w);
+	free(w);
+}
+
+/**
  * Accept all the client connections
  */
 static void accept_callback(EV_P, ev_io *w, int revents) {
-	// TODO: accept client connections and them to them IDs
 	// TODO: check for EV_ERROR & revents
+
+	struct sockaddr_in address;
+	socklen_t client_len = sizeof(address);
+	struct ev_io *client = (struct ev_io*) malloc (sizeof(struct ev_io));
+
+	int sock = accept(w->fd, (struct sockaddr *)&address, &client_len);
+
+	ev_io_init(client, write_callback, sock, EV_WRITE);
+	ev_io_start(loop, client);
+
 }
 
 /**
@@ -46,7 +68,7 @@ int server_serve(int port) {
 
 	// Initialize and start IO watcher
 	// `accept_callback` will be called when sock will become writeable
-	ev_io_init(&watcher, accept_callback, sock, EV_WRITE);
+	ev_io_init(&watcher, accept_callback, sock, EV_READ);
 	ev_io_start(loop, &watcher);
 	printf("Start\n");
 	// Run loop
